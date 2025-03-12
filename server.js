8741edca8efb1daac04af53c0d1b190e54ad81f8
AreebAstute <areeb@astutesoftwares.com>
2025-03-12 13:27:41 +0500

    added .env file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 const isTest = process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD;
 process.env.MY_CUSTOM_SECRET = 'API_KEY_qwertyuiop';
 
-async function createServer(root = process.cwd(), isProd = process.env.NODE_ENV === 'production') {
+async function createServer(root = process.cwd(), isProd = process.env.VITE_NODE_ENV === process.env.VITE_NODE_ENV) {
   const resolve = (p) => path.resolve(__dirname, p);
 
   console.log("this is resolve", resolve)
